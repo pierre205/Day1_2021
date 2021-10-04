12b8c79835241ab94fb5c819cc8c31a31fca34b8
pierre <pierre-alexandre.cochet@epitech.eu>
2021-10-04 16:20:08 +0200

    day1

---
+++ push_that.sh
@@ -0,0 +1,4 @@
+blih -u pierre-alexandre.cochet@epitech.eu repository create $1
+blih -u pierre-alexandre.cochet@epitech.eu repository setacl $1 ramassage-tek r
+blih -u pierre-alexandre.cochet@epitech.eu repository getacl $1
+
